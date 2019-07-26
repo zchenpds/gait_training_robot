d8937d2a152adebc5b92267de7c0cb489cf3e68a
Zhuo Chen <zchenpds@gmail.com>
2019-07-26 19:03:00 -0400

    Tune PI controller parameters

--- src/distance_controller.cpp
+++ src/distance_controller.cpp
@@ -40,8 +40,13 @@ void DistanceController::cmdVelCB(const geometry_msgs::Twist & cmd_vel_in)
     //ros::Duration(1.0).sleep();
   }
 
-  ros::Duration time_since_last_update = ros::Time::now() - estimated_state_.stamp;
-  if (time_since_last_update < ros::Duration(0.8)) 
+  const ros::Time stamp_now = ros::Time::now();
+  ros::Duration time_since_last_state_update = stamp_now - estimated_state_.stamp;
+  static ros::Time stamp_last_control_update = stamp_now;
+  ros::Duration time_since_last_control_update = stamp_now - stamp_last_control_update;
+  stamp_last_control_update = stamp_now;
+
+  if (time_since_last_state_update < ros::Duration(0.8)) 
   {
     // Looks like the human state is up-to-date
     if (cmd_vel_in.linear.x > 0.2)
@@ -50,14 +55,14 @@ void DistanceController::cmdVelCB(const geometry_msgs::Twist & cmd_vel_in)
       // Presumably no obstacles ahead and far from the next goal point.
       // Use a PI controller to maintain the distance.
       HumanState error_state_ = desired_state_ - estimated_state_;
-      double K_p = 0.0;
-      double K_i = 0.0;
+      double K_p = 1.0;
+      double K_i = 0.5;
       double v_nominal = 0.4;
       static double u_i;
-      u_i += K_i * error_state_.distance;
+      u_i += K_i * error_state_.distance * clamp(time_since_last_control_update.toSec(), 0.0, 0.2);
       u_i = clamp(u_i, -0.4, 0.4);
       double u_p = K_p * error_state_.distance;
-      cmd_vel_out.linear.x = clamp(v_nominal- u_p - u_i, 0.0, 0.7);
+      cmd_vel_out.linear.x = clamp(v_nominal + u_p + u_i, 0.0, 0.7);
       cmd_vel_out.angular.z = cmd_vel_in.angular.z / cmd_vel_in.linear.x * cmd_vel_out.linear.x ;
       ROS_INFO_STREAM_NAMED("DistanceController", "u_p: " << u_p << ", u_i: " << u_i 
         << ", v_out" << cmd_vel_out.linear.x );
