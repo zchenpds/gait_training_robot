21ded4f4b618599a73780febdce10ab7945d12bb
Zhuo Chen <zchenpds@gmail.com>
2020-05-11 21:39:51 -0400

    fix open_loop_controller bugs

--- src/open_loop_controller.py
+++ src/open_loop_controller.py
@@ -13,64 +13,85 @@ def main():
     rospy.init_node('open_loop_controller')
 
     # Get parameters
-    mode = rospy.get_param('mode', default='straight')
-    terminal_distance = rospy.get_param('terminal_distance', default=3.0) # Desired distance
-    v_max = rospy.get_param('v_max', default = 0.7)
+    mode = rospy.get_param('~mode', default='straight')
+    terminal_distance = rospy.get_param('~terminal_distance', default=3.0) # Desired distance
+    v_max = rospy.get_param('~v_max', default = 0.7)
 
     if mode != 'straight':
         rospy.logerr('Unknown mode %s' % mode)
         return
+
+    # Define stage
+    stage = 0
     
     # Create Transform Listener
     listener = tf.TransformListener()
     distance = 0.0
 
     # Create cmd_vel publisher
-    pub_cmd_vel = rospy.Publisher('/Pioneer3AT/cmd_vel', geometry_msgs.msg.Twist, queue_size=1)
+    pub_cmd_vel = rospy.Publisher('/cmd_vel', geometry_msgs.msg.Twist, queue_size=1)
     msg_cmd_vel = geometry_msgs.msg.Twist()
 
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
         try:
-            (trans,rot) = listener.lookupTransform('/base_link', '/odom', rospy.Time(0))
+            (trans,rot) = listener.lookupTransform('/odom', '/base_link', rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
 
         # Update the distance traveled
         if 'trans1' in locals():
-            distance += math.hypot(trans[0] - trans1[0], trans[1] - trans1[1])
+            dx = trans[0] - trans1[0]
+            dy = trans[1] - trans1[1]
+            dth = math.atan2(dy, dx)
+            distance_inc = math.hypot(dx, dy)
+            distance += distance_inc * math.fabs(math.cos(dth - rot[2]))
         else:
             distance = 0
+        (trans1, rot1) = (trans, rot)
+        # rospy.loginfo('Distance:  %f; x = %f, y = %f.' % (distance, trans[0], trans[1]) )
 
         # Calculate the desired velocity
-        e = max(terminal_distance - distance, 0.0)
-        kp = 0.8
-        u = clamp( kp * math.sqrt( 2.0 * 0.3 * e), 0.0, v_max)
+        if stage == 0:
+            # Move forward
+            e = max(terminal_distance - distance, 0.0)
+            kp = 0.9
+            u = clamp( kp * math.sqrt( 0.9 * 2.0 * 0.3 * e), 0.0, v_max)
+            if e < 0.01:
+                u = 0.0
+                stage += 1
+                t0 = rospy.Time.now()
+        elif stage == 1:
+            # Wait for 10 secs
+            duration_waited = rospy.Time.now() - t0
+            if duration_waited.to_sec() > 10.0:
+                stage += 1
+                distance = 0
+        elif stage == 2:
+            # Move backward
+            e = max(terminal_distance - distance, 0.0)
+            kp = 0.8
+            u = -clamp( kp * math.sqrt( 2.0 * 0.3 * e), 0.0, v_max/2.0)
+            if e < 0.01:
+                u = 0.0
+                stage += 1
+        else:
+            #Time to shutdown
+            pass
 
         # Populate the message and publish
         msg_cmd_vel.linear.x = u
-        msg_cmd_vel.linear.y = 0.0
-        msg_cmd_vel.linear.z = 0.0
-        msg_cmd_vel.angular.x = 0.0
-        msg_cmd_vel.angular.y = 0.0
-        msg_cmd_vel.angular.z = 0.0
         pub_cmd_vel.publish(msg_cmd_vel)
 
         # Terminate if error becomes smaller than tolerance
-        if e < 0.2:
+        if stage > 2:
             break
 
         # Prepare for next iteration
-        (trans1, rot1) = (trans, rot)
         rate.sleep()
 
     # Reset cmd_vel before exit
     msg_cmd_vel.linear.x = 0.0
-    msg_cmd_vel.linear.y = 0.0
-    msg_cmd_vel.linear.z = 0.0
-    msg_cmd_vel.angular.x = 0.0
-    msg_cmd_vel.angular.y = 0.0
-    msg_cmd_vel.angular.z = 0.0
     pub_cmd_vel.publish(msg_cmd_vel)
     rospy.loginfo('cmd_vel set to (0.0, 0.0)')
 
