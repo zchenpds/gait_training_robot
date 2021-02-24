2b69bdf265f5c183333f969dd5c72f59810a0f7d
Zhuo Chen <zchenpds@gmail.com>
2021-02-23 21:18:53 -0500

    Add kinect_pose_estimator node

---
+++ include/gait_training_robot/kinect_pose_estimator.h
@@ -0,0 +1,76 @@
+#ifndef KINECT_POSE_ESTIMATOR_H
+#define KINECT_POSE_ESTIMATOR_H
+
+
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Imu.h>
+
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/utils.h>
+
+#include <message_filters/cache.h>
+#include <message_filters/subscriber.h>
+
+class KinectPoseEstimator
+{
+public:
+  KinectPoseEstimator(const ros::NodeHandle& n = ros::NodeHandle(), const ros::NodeHandle& p = ros::NodeHandle("~"));
+  ~KinectPoseEstimator();
+  void odomCB(const nav_msgs::Odometry & msg);
+  void kimuCB(const sensor_msgs::Imu & msg);
+  void broadcastTf();
+
+private:
+  // node_handle
+  ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_;
+
+  // tf
+  tf2_ros::TransformBroadcaster tf_broadcaster_;
+  geometry_msgs::TransformStamped pose_estimate_;
+
+  // State
+  struct State
+  {
+    bool ready = false;
+    ros::Time t;
+    double x;
+    double y;
+    double th;
+    double vx;
+    double bax;
+    double bwz;
+    void predict(double ax, double wz, double dt);
+    State& operator=(const State& rhs);
+  } state_, state_posterior_;
+  
+
+  // Subscribers
+  message_filters::Subscriber<sensor_msgs::Imu> sub_kimu_;
+  message_filters::Cache<sensor_msgs::Imu> cache_kimu_;
+  message_filters::Subscriber<nav_msgs::Odometry> sub_odom_;
+  message_filters::Cache<nav_msgs::Odometry> cache_odom_;
+
+  // Publishers
+  ros::Publisher pub_filtered_odom_;
+  nav_msgs::Odometry odom_filtered_;
+
+  // ts
+  ros::Time ts_odom_last_;
+  ros::Time ts_kimu_last_;
+  ros::Time ts_init_;
+  ros::Time ts_next_desired_publish_;
+
+  // params
+  const ros::Duration desired_publish_period_{1.0 / 100.0};
+};
+
+template<typename T>
+T wrapToPi(T angle)
+{
+  return atan2(sin(angle), cos(angle));
+}
+
+
+#endif // KINECT_POSE_ESTIMATOR_H
---
+++ src/kinect_pose_estimator.cpp
@@ -0,0 +1,213 @@
+#include "gait_training_robot/kinect_pose_estimator.h"
+#include <iomanip>
+
+
+KinectPoseEstimator::KinectPoseEstimator(const ros::NodeHandle& n, const ros::NodeHandle& p) :
+    nh_(n),
+    private_nh_(p),
+    sub_kimu_(nh_, "/imu", 100),
+    cache_kimu_(sub_kimu_, 800),
+    sub_odom_(nh_, "/odom", 3),
+    cache_odom_(sub_odom_, 6)
+{
+  cache_kimu_.registerCallback(&KinectPoseEstimator::kimuCB, this);
+  cache_odom_.registerCallback(&KinectPoseEstimator::odomCB, this);
+  
+  pub_filtered_odom_ = private_nh_.advertise<nav_msgs::Odometry>("odom", 20);
+  
+  // Initialize tf message
+  pose_estimate_.header.frame_id = "odom";
+  pose_estimate_.child_frame_id = "base_link";
+
+  // Initialize filtered odom message
+  odom_filtered_.header.frame_id = "odom";
+  odom_filtered_.child_frame_id = "base_link";
+}
+
+KinectPoseEstimator::~KinectPoseEstimator()
+{
+}
+
+
+void KinectPoseEstimator::odomCB(const nav_msgs::Odometry & msg)
+{
+  if (ts_odom_last_.isZero() && !ts_kimu_last_.isZero())
+  {
+    // State Initialization
+    state_.t = ts_kimu_last_;
+    state_.x = msg.pose.pose.position.x;
+    state_.y = msg.pose.pose.position.y;
+    state_.th = tf2::getYaw(msg.pose.pose.orientation);
+    state_.vx = msg.twist.twist.linear.x;
+    state_.bax = 0.0;
+    state_.bwz = 0.0;
+    pose_estimate_.transform.rotation = msg.pose.pose.orientation;
+    state_.ready = true;
+    ts_init_ = ts_odom_last_ = msg.header.stamp;
+    state_posterior_ = state_;
+  }
+}
+
+
+void KinectPoseEstimator::kimuCB(const sensor_msgs::Imu & msg)
+{
+  // Proceed only if the state is initialized with an odom message.
+  if (ts_odom_last_.isZero()) 
+  {
+    ts_kimu_last_ = msg.header.stamp;
+    return;
+  }
+
+  static int num_of_predictions_between_updates;
+  // Predict only 0.1 sec into the future from the previuos measurement
+  if (msg.header.stamp < ts_odom_last_ + ros::Duration(0.098))
+  {
+    auto msg_ptrs = cache_kimu_.getInterval(ts_kimu_last_, msg.header.stamp);
+    
+    num_of_predictions_between_updates = 0;
+    for (auto msg_ptr : msg_ptrs)
+    {
+      if (msg_ptr->header.stamp == ts_kimu_last_) continue;
+
+      ++num_of_predictions_between_updates;
+      double dt = (msg_ptr->header.stamp - ts_kimu_last_).toSec();
+      // ROS_INFO_STREAM("Predicting state: " << state_.x << " " << state_.y << " " << state_.th << " for " << dt);
+      state_.predict(msg_ptr->linear_acceleration.x, -msg_ptr->angular_velocity.z, dt);
+      broadcastTf();
+      // ROS_INFO_STREAM("Predicted state: " << state_.x << " " << state_.y << " " << state_.th << " at " << state_.t.toSec());
+      ts_kimu_last_ = msg_ptr->header.stamp;
+    }
+
+  }
+  else
+  {
+    // Update:
+    auto msg_odom_ptr = cache_odom_.getElemAfterTime(ts_odom_last_);
+    if (!msg_odom_ptr) return;
+    
+    double z_x = msg_odom_ptr->pose.pose.position.x;
+    double z_y = msg_odom_ptr->pose.pose.position.y;
+    double z_vx = msg_odom_ptr->twist.twist.linear.x;
+    double z_th = tf2::getYaw(msg_odom_ptr->pose.pose.orientation);
+
+    auto msg_odom_ptr_prev = cache_odom_.getElemAfterTime(ts_odom_last_ - ros::Duration(0.01));
+    if (msg_odom_ptr_prev)
+    {
+      double z_dt = (msg_odom_ptr->header.stamp - msg_odom_ptr_prev->header.stamp).toSec();
+      double z_vx_1 = msg_odom_ptr_prev->twist.twist.linear.x;
+      double z_th_1 = tf2::getYaw(msg_odom_ptr_prev->pose.pose.orientation);
+
+      const double K_bax = 0.1;
+      const double K_bwz = 0.05;
+
+      state_.bax += K_bax *
+          ( (state_.vx - state_posterior_.vx) / (state_.t - state_posterior_.t).toSec() - 
+            (z_vx - z_vx_1) / z_dt
+          );
+      state_.bwz += K_bwz *
+          ( wrapToPi(state_.th - state_posterior_.th) / (state_.t - state_posterior_.t).toSec() -
+            wrapToPi(z_th - z_th_1) / z_dt
+          );
+    }
+    else
+    {
+      ROS_WARN("Impossible! msg_odom_ptr_prev == nullptr! This shouldn't happen!");
+    }
+
+    const double K = 0.1;
+    state_.vx = state_.vx + K * (z_vx - state_.vx);
+  
+    state_.x = state_.x + K * (z_x - state_.x);
+    state_.y = state_.y + K * (z_y - state_.y);
+    state_.th = state_.th + 0.02 * wrapToPi(z_th - state_.th);
+    // state_.th = wrapToPi(state_.th);
+
+    // Debug output
+    static bool once = [](){
+      printf("|   ts   |  bax   |   vx   |  bwz   |   th   |  cnt   |\n");
+      printf("|--------+--------+--------+--------+--------+--------|\n");
+      return true;
+    }();
+    printf("|%8.4lf|%8.4lf|%8.4lf|%8.4lf|%8.4lf|%8d|\r", 
+      (state_.t - ts_init_).toSec(), state_.bax, state_.vx, state_.bwz, state_.th, num_of_predictions_between_updates);
+    fflush(stdout);
+
+    // Update for next iteration
+    ts_odom_last_ = msg_odom_ptr->header.stamp;
+    state_posterior_ = state_;
+    state_.ready = true;
+  }
+
+
+}
+
+void KinectPoseEstimator::broadcastTf()
+{
+
+  if (state_.t < ts_next_desired_publish_) return;
+
+  pose_estimate_.header.seq++;
+  pose_estimate_.header.stamp = state_.t;
+  pose_estimate_.transform.translation.x = state_.x;
+  pose_estimate_.transform.translation.y = state_.y;
+  tf2::Quaternion quat_yaw;
+  quat_yaw.setRPY(0.0, 0.0, state_.th);
+  tf2::convert(quat_yaw, pose_estimate_.transform.rotation);
+  // ROS_INFO_STREAM("Sending tf: " << state_.x << " " << state_.y << " " << state_.th << " at " << state_.t.toSec());
+  tf_broadcaster_.sendTransform(pose_estimate_);
+
+  state_.ready = false;
+  if (state_.t < ts_next_desired_publish_ + desired_publish_period_)
+  {
+    ts_next_desired_publish_ += desired_publish_period_;
+  }
+  else
+  {
+    ts_next_desired_publish_ = state_.t + desired_publish_period_;
+  }
+
+  // Debug output
+  odom_filtered_.header.seq++;
+  odom_filtered_.header.stamp = state_.t;
+  odom_filtered_.pose.pose.position.x = state_.x;
+  odom_filtered_.pose.pose.position.y = state_.y;
+  odom_filtered_.twist.twist.linear.x = state_.vx;
+  odom_filtered_.pose.pose.orientation = pose_estimate_.transform.rotation;
+  pub_filtered_odom_.publish(odom_filtered_);
+
+
+}
+
+void KinectPoseEstimator::State::predict(double ax, double wz, double dt)
+{
+  vx += (ax - bax) * dt;
+  x += vx * cos(th) * dt;
+  y += vx * sin(th) * dt;
+  th += (wz - bwz) * dt;
+  // th = wrapToPi(th);
+  t += ros::Duration(dt);
+  ready = true;
+}
+
+KinectPoseEstimator::State& KinectPoseEstimator::State::operator=(const KinectPoseEstimator::State& rhs)
+{
+  ready = rhs.ready;
+  t = rhs.t;
+  x = rhs.x;
+  y = rhs.y;
+  th = rhs.th;
+  vx = rhs.vx;
+  bax = rhs.bax;
+  bwz = rhs.bwz;
+}
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "kinect_pose_estimator");
+  KinectPoseEstimator kpe;
+
+  ros::spin();
+  std::cout << std::endl;
+  return 0;
+}
